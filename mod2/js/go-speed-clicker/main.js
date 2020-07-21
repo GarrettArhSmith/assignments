2c225566b98a2b5c74bf9f43f779f7224d79c9c6
Garrett Smith <garrettarhsmith@gmail.com>
2020-07-21 17:20:00 -0400

    Fourth Commit

---
+++ mod2/js/go-speed-clicker/main.js
@@ -0,0 +1,26 @@
+tracker = document.createElement("h1")
+tracker.textContent = localStorage.getItem("clicks")
+document.body.append(tracker)
+
+let timeLeft = 30
+timer = document.createElement("h1")
+timer.textContent = timeLeft
+document.body.append(timer)
+
+const countDown = setInterval(() => {
+    timeLeft -= 1
+    timer.textContent = timeLeft
+    if (timeLeft <= 0) clearInterval(countDown)
+}, 1000)
+
+
+
+let clicks
+window.addEventListener("click", e => {
+    if (e.target.type === "BUTTON") clearInterval(countDown)
+    else if (timeLeft > 0){
+        clicks = Number(localStorage.getItem("clicks"))
+        localStorage.setItem("clicks", ++clicks)
+        tracker.textContent = localStorage.getItem("clicks")
+    }
+})
